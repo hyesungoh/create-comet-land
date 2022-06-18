2a784e317f6f516676160769dbbd0b7fc513a434
Hyesung Oh <haesungoh414@gmail.com>
2022-06-19 02:33:12 +0900

    feat: checking installed git before clone project

--- dist/ui.js
+++ dist/ui.js
@@ -3,11 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const ink_1 = require("ink");
-var a = 1;
-let b = 1;
-const App = ({ name = 'Stranger' }) => (react_1.default.createElement(ink_1.Text, null,
-    "Helslo, ",
-    react_1.default.createElement(ink_1.Text, { color: "green" }, name)));
+var react_1 = __importDefault(require("react"));
+var ink_1 = require("ink");
+function App(_a) {
+    var _b = _a.name, name = _b === void 0 ? 'Stranger' : _b;
+    return (react_1.default.createElement(ink_1.Text, null,
+        "Hello, ",
+        react_1.default.createElement(ink_1.Text, { color: "green" }, name)));
+}
 exports.default = App;
