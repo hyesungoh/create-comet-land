a1546e2aa7e45cdd9db86ef9091278dad17a3428
Hyesung Oh <haesungoh414@gmail.com>
2022-08-01 03:03:40 +0900

    feat: delete invalid files at third section

--- source/3_Delete_invalid_files.tsx
+++ source/3_Delete_invalid_files.tsx
@@ -5,11 +5,25 @@ import shell from 'shelljs';
 
 import Configuration from './4_Configuration';
 
+const BASE_DIRECTORY = './comet-land/';
+
+const INVALID_FILES = [
+  '.git',
+  '.github/workflows/CI.yml',
+  '.github/codecov.yml',
+  '.github/CODEOWNERS',
+  'LICENSE',
+  'CONTRIBUTING.md',
+  'CODE_OF_CONDUCT.md',
+] as const;
+
+const DELETE_FILES = INVALID_FILES.map(directory => `${BASE_DIRECTORY}${directory}`).join(' ');
+
 export default function DeleteInvalidFiles() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    shell.exec('rm -rf ./comet-land/.git', { silent: true }, () => {
+    shell.exec(`rm -rf ${DELETE_FILES}`, { silent: true }, () => {
       setIsLoading(false);
     });
   }, []);
